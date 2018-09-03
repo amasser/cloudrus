8821843aeca54ad46f2ac49b4dc2d7ae25c9e151
Robin Temme <zwerch1337@googlemail.com>
2018-09-03 18:32:29 +0200

    Implement the hook
    Currently only works for batchFrequency=0.

--- hook.go
+++ hook.go
@@ -1,35 +1,156 @@
 package cloudrus
 
 import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+
+	"github.com/sirupsen/logrus"
 )
 
 // Hook represents one hook
 type Hook struct {
-	Session   *cloudwatchlogs.CloudWatchLogs
-	LogGroup  string
-	LogStream string
+	service *cloudwatchlogs.CloudWatchLogs
+
+	inputLogEvents    chan *cloudwatchlogs.InputLogEvent
+	nextSequenceToken *string
+	mutex             sync.Mutex
+	err               chan error
+
+	groupName  string
+	streamName string
 }
 
 // NewHook creates a new hook with an AWS session taken from the environment
-func NewHook(logGroup string, logStream string) (hook *Hook, err error) {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
+func NewHook(groupName string, streamName string, batchFrequency time.Duration) (hook *Hook, err error) {
+	sess := session.Must(session.NewSession())
 
-	hook.LogGroup = logGroup
-	hook.LogStream = logStream
-	hook, err = NewHookWithSession(logGroup, logStream, sess)
+	hook, err = NewHookWithSession(groupName, streamName, batchFrequency, sess)
 
 	return
 }
 
 // NewHookWithSession creates a new hook with a custom AWS session
-func NewHookWithSession(logGroup string, logStream string, sess *session.Session) (hook *Hook, err error) {
-	hook.LogGroup = logGroup
-	hook.LogStream = logStream
-	hook.Session = cloudwatchlogs.New(sess)
+func NewHookWithSession(groupName string, streamName string, batchFrequency time.Duration, sess *session.Session) (hook *Hook, err error) {
+	// init the hook
+	h := Hook{
+		service:    cloudwatchlogs.New(sess),
+		groupName:  groupName,
+		streamName: streamName,
+	}
 
-	return
+	// set the log stream
+	resp, err := h.service.DescribeLogStreams(&cloudwatchlogs.DescribeLogStreamsInput{
+		LogGroupName:        aws.String(h.groupName),
+		LogStreamNamePrefix: aws.String(h.streamName),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// get the first sequence token
+	if len(resp.LogStreams) > 0 {
+		h.nextSequenceToken = resp.LogStreams[0].UploadSequenceToken
+		return &h, nil
+	}
+
+	// if the batch frequency is above 0
+	if batchFrequency > 0 {
+		// maximum number of entries is 10000
+		h.inputLogEvents = make(chan *cloudwatchlogs.InputLogEvent, 10000)
+		// start the ticker
+		go h.putBatches(time.Tick(batchFrequency))
+	}
+
+	return &h, nil
+}
+
+// Fire adds an entry
+func (h *Hook) Fire(e *logrus.Entry) error {
+	// use the json formatter for the entry
+	json := &logrus.JSONFormatter{}
+	buf, err := json.Format(e)
+	if err != nil {
+		return err
+	}
+	msg := string(buf)
+
+	// create the event
+	event := &cloudwatchlogs.InputLogEvent{
+		Message:   aws.String(msg),
+		Timestamp: aws.Int64(int64(e.Time.UnixNano()) / int64(time.Millisecond)),
+	}
+
+	// if batch frequency is set
+	if h.inputLogEvents != nil {
+		// add the event to the event channel
+		h.inputLogEvents <- event
+		if lastErr, ok := <-h.err; ok {
+			return fmt.Errorf("%v", lastErr)
+		}
+	} else {
+		// lame normal synchronous put
+
+		// use a mutex to prevent race conditions
+		h.mutex.Lock()
+		defer h.mutex.Unlock()
+
+		// create the event
+		params := &cloudwatchlogs.PutLogEventsInput{
+			LogEvents:     []*cloudwatchlogs.InputLogEvent{event},
+			LogGroupName:  aws.String(h.groupName),
+			LogStreamName: aws.String(h.streamName),
+			SequenceToken: h.nextSequenceToken,
+		}
+		// put the events into the log stream
+		resp, err := h.service.PutLogEvents(params)
+
+		if err != nil {
+			return err
+		}
+
+		// get the next sequence token
+		h.nextSequenceToken = resp.NextSequenceToken
+
+	}
+
+	return nil
+}
+
+// putBatches puts the batches into the log stream synchronized by the ticker
+func (h *Hook) putBatches(ticker <-chan time.Time) {
+	var batch []*cloudwatchlogs.InputLogEvent
+	for {
+		select {
+		case p := <-h.inputLogEvents:
+			// add the events from the channel to the batch
+			batch = append(batch, p)
+		case <-ticker:
+			// when ticking, create the events
+			params := &cloudwatchlogs.PutLogEventsInput{
+				LogEvents:     batch,
+				LogGroupName:  aws.String(h.groupName),
+				LogStreamName: aws.String(h.streamName),
+				SequenceToken: h.nextSequenceToken,
+			}
+			// put the batched events into the stream
+			resp, err := h.service.PutLogEvents(params)
+			if err != nil {
+				h.err <- err
+			} else {
+				// fetch the next sequence token and
+				h.nextSequenceToken = resp.NextSequenceToken
+				batch = nil
+			}
+		}
+	}
+}
+
+// Levels returns all the levels
+func (h *Hook) Levels() []logrus.Level {
+	return logrus.AllLevels
 }
